de232ddea7e3df1bd64d63a9a604d70c3adffe13
Verber <vadimartamonov643@gmail.com>
2023-11-04 22:16:15 +0500

    feature(main.py): вынес считывание страницы в отдельную функцию, а также добавил обработку поля "текущий сет", если вместо номера сета приходит занчение "Ожидание обновления"

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ import pandas as pd
 from prettytable import PrettyTable
 import time
 
+# Считываем данные с flashscore возвращаем данные со страницы с live-матчами
+def read_web_page():
+    url = "https://www.flashscorekz.com/table-tennis/"
+    geckodriver_path = r'D:\src_git\flashscore parse\firefoxdriver\geckodriver.exe'
 
-url = "https://www.flashscorekz.com/table-tennis/"
+    #url = 'https://tennis-score.pro/live_v2/' #Лига ПРО
 
-# Заполняем таблицу с результатами матчей
-def get_dataframe():
-
-    service = Service(executable_path=r'D:\src_git\flashscore parse\firefoxdriver\geckodriver.exe')
+    service = Service(executable_path=geckodriver_path)
     driver = webdriver.Firefox(service=service)
     driver.get(url)
 
     time.sleep(3)
 
-    all_rows = driver.find_elements("xpath", "//div[@class='event__match event__match--live event__match--twoLine']")
-    print('Количество лайв-матчей:', len(all_rows), '\n')
+    # Находим все текущие live-матчи
+    data_rows = driver.find_elements("xpath", "//div[@class='event__match event__match--live event__match--twoLine']")
+
+    return data_rows
+
+
+# Заполняем таблицу с результатами матчей
+def get_dataframe(data_rows):
+
+    print('Количество лайв-матчей:', len(data_rows), '\n')
 
     # Список полей
     # Текущий сет (Пример вывода: 3)
@@ -51,10 +60,8 @@ def get_dataframe():
     # Игрок 2: число очков в 5-м сете
     points_away_5_set = []
 
-    for row in all_rows:
-        # Текущий сет (Пример вывода: 3-й сет) (достаем из строки только номер текущего сета)
-        num_cur_set = int(row.find_element("xpath", ".//div[@class='event__stage']").text[0])
-        cur_set.append(num_cur_set)
+    for row in data_rows:
+        # Игрок 1 (Пример вывода: Иванов А.)
         participant_home.append(row.find_element("xpath",
                                             ".//div[@class='event__participant event__participant--home']").text)
         # Игрок 2 (Пример вывода: Иванов И.)
@@ -64,6 +71,16 @@ def get_dataframe():
         score_home.append(int(row.find_element("xpath", ".//div[@class='event__score event__score--home']").text))
         # Количество выигранных сетов 2-ого игрока (Пример вывода: 2)
         score_away.append(int(row.find_element("xpath", ".//div[@class='event__score event__score--away']").text))
+
+        # Текущий сет (Пример вывода: 3-й сет) (достаем из строки только номер текущего сета)
+        # Так как это поле может содержать значение "Ожидание обновления", то в этом случае номер
+        # текущего сета определяем на основе количества выигранных сетов
+        num_cur_set = 0
+        try:
+            num_cur_set = int(row.find_element("xpath", ".//div[@class='event__stage']").text[0])
+        except Exception:
+            num_cur_set = score_home[-1] + score_away[-1] + 1
+        cur_set.append(num_cur_set)
 
         #for num_set in range(1, num_cur_set + 1): # сделать как-то через цикл
             #locals()["points_home_" + str(num_set) + "_set"].append(...)
@@ -190,8 +207,10 @@ def print_matchs_from_dataframe(dataframe):
 
 
 def main():
+    # Считываем данные с flashscore возвращаем данные со страницы с live-матчами
+    data_rows = read_web_page()
     # Получаем датафрейм с данными лайв-матчей в настольном теннисе
-    df = get_dataframe()
+    df = get_dataframe(data_rows)
     # Печатаем каждый матч из датафрейма в виде таблицы
     print_matchs_from_dataframe(df)
 
